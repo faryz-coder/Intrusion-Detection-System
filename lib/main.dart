d2f84ea83fc14e008d62cbd67586629975239fac
faryz-coder <faryz.coder@gmail.com>
2023-09-09 17:20:09 +0800

    Integrate Device Preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
+import 'dart:js';
+
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      builder: (context) => const MyApp(),
+      enabled: true,
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
