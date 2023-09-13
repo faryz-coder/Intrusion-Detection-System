b78cb0e38729a4cd42707edcc43ec016907ac82e
faryz-coder <faryz.coder@gmail.com>
2023-09-13 22:03:50 +0800

    Display List Network

--- lib/Screen/NetworkScanning/networkScanningScreen.dart
+++ lib/Screen/NetworkScanning/networkScanningScreen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 class NetworkScanningScreen extends StatelessWidget {
-  const NetworkScanningScreen({super.key});
+  NetworkScanningScreen({super.key});
+
+  final List<String> items = <String>["network 1", "network 2"];
+
 
   @override
   Widget build(BuildContext context) {
@@ -9,7 +12,57 @@ class NetworkScanningScreen extends StatelessWidget {
       body: Container(
         color: Colors.red,
         alignment: Alignment.center,
-        child: const Text('page 1'),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 56,
+            ),
+            Card(
+              color: Theme.of(context).colorScheme.surfaceVariant,
+              child: SizedBox(
+                width: double.infinity,
+                height: 100,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('Connected To'),
+                    Text('STATUS'),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+              child: Card(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ListView.builder(
+                      padding: const EdgeInsets.all(0),
+                      itemCount: items.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          margin: const EdgeInsets.only(bottom: 5.0),
+                          child: Card(
+                            child: SizedBox(
+                              width: double.infinity,
+                              height: 50,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [Text((items[index]))],
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      }),
+                ),
+              ),
+            ),
+            SizedBox(height: 5),
+          ],
+        ),
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -68,10 +68,6 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text("any"),
-      ),
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
           setState(() {
@@ -92,7 +88,7 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ],
       ),
-      body: const <Widget>[
+      body:  <Widget>[
         NetworkScanningScreen(),
         ControlSystemScreen(),
       ][currentPageIndex],
